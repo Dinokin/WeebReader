b95ddcc3a0424dc93a3a52968778b8cac0e45eab
Micael Valentim <lord.valentim@gmail.com>
2020-01-05 12:29:23 -0300

    Package inherit from resource

--- Entities/Package.cs
+++ Entities/Package.cs
@@ -3,10 +3,8 @@ using WeebReader.Data.Entities.Abstract;
 
 namespace WeebReader.Data.Entities
 {
-    public class Package : BaseEntity
+    public class Package : Resource
     {
-        public string Name { get; set; }
-        public byte[] Content { get; set; }
         public Guid ChapterId { get; set; }
         
         public Chapter Chapter { get; set; }
