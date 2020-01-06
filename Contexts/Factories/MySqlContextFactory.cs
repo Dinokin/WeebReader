888f831e4b3ad79e37c234e4ba40685e1349f32e
Micael Valentim <lord.valentim@gmail.com>
2020-01-06 11:10:30 -0300

    Add MySqlContext and the factory needed for efcore migrations

---
+++ Contexts/Factories/MySqlContextFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace WeebReader.Data.Contexts.Factories
+{
+    public class MySqlContextFactory : IDesignTimeDbContextFactory<MySqlContext>
+    {
+        public MySqlContext CreateDbContext(string[] args)
+        {
+            return new MySqlContext(new DbContextOptionsBuilder().UseMySql("Server=localhost;Database=ef;User=root;Password=123456;").Options);
+        }
+    }
+}
