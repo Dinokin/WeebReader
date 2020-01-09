552a98f20013a019a3baf5124715ac0d205aae95
Micael Valentim <lord.valentim@gmail.com>
2020-01-09 20:31:10 -0300

    Add required libraries and settings

--- Portal/Startup.cs
+++ Portal/Startup.cs
@@ -1,15 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WeebReader.Data.Contexts;
+using WeebReader.Data.Contexts.Abstract;
 
 namespace WeebReader.Web.Portal
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration) => _configuration = configuration;
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MariaDbContext>(builder => builder.UseMySql(_configuration.GetConnectionString("MariaDb")));
+
+            services.AddIdentity<IdentityUser<Guid>, IdentityRole<Guid>>(options =>
+                {
+                    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromHours(3);
+                    options.Lockout.MaxFailedAccessAttempts = 3;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequiredLength = 8;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.User.RequireUniqueEmail = true;
+                })
+                .AddDefaultTokenProviders().AddEntityFrameworkStores<BaseContext>();
+
+            services.AddRouting(options =>
+            {
+                options.LowercaseUrls = true;
+                options.LowercaseQueryStrings = true;
+            });
+            
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/admin/";
+                options.LogoutPath = "/admin/signout";
+                options.AccessDeniedPath = "/denied";
+            });
+            
+            services.AddDataProtection().PersistKeysToFileSystem(Directory.CreateDirectory($"{Utilities.CurrentDirectory}{Path.DirectorySeparatorChar}Keys"))
+                .ProtectKeysWithCertificate(Utilities.GetCertificate());
+
+            services.AddControllersWithViews()
+                .AddViewOptions(options => options.HtmlHelperOptions.ClientValidationEnabled = true);
         }
         
         public void Configure(IApplicationBuilder application, IWebHostEnvironment environment)
@@ -17,11 +65,30 @@ namespace WeebReader.Web.Portal
             if (environment.IsDevelopment())
             {
                 application.UseDeveloperExceptionPage();
+                application.UseDatabaseErrorPage();
             }
-
+            else
+                application.UseExceptionHandler("/error");
+            
+            application.UseRequestLocalization(options =>
+            {
+                options.DefaultRequestCulture = new RequestCulture(CultureInfo.InvariantCulture);
+                options.SupportedCultures = new List<CultureInfo>(new[] {CultureInfo.InvariantCulture});
+                options.SupportedUICultures = new List<CultureInfo>(new[] {CultureInfo.InvariantCulture});
+            });
+            
+            application.UseStaticFiles();
             application.UseRouting();
+            application.UseAuthentication();
+            application.UseAuthorization();
 
-            application.UseEndpoints(endpoints => { endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); }); });
+            application.UseEndpoints(builder => builder.MapDefaultControllerRoute());
+
+            using var scope = application.ApplicationServices.CreateScope();
+            using var context = scope.ServiceProvider.GetRequiredService<BaseContext>();
+            
+            if (context.Database.GetPendingMigrations().Any())
+                context.Database.Migrate();
         }
     }
 }
