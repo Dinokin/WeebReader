bd0476f501a15701955facb46e60184e5b38678c
Micael Valentim <lord.valentim@gmail.com>
2020-06-23 18:38:24 -0300

    Add support for single file builds

--- Common.Utilities/Location.cs
+++ Common.Utilities/Location.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.IO;
-using System.Reflection;
 
 namespace WeebReader.Common.Utilities
 {
     public static class Location
     {
-        public static DirectoryInfo CurrentDirectory => new FileInfo(Uri.UnescapeDataString(new Uri(Assembly.GetExecutingAssembly().CodeBase!).AbsolutePath)).Directory;
+        public static DirectoryInfo CurrentDirectory => new DirectoryInfo(Environment.CurrentDirectory);
     }
 }
