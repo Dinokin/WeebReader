03a50111a6cdd9d10330253506cf51527166b009
Micael Valentim <lord.valentim@gmail.com>
2020-07-01 14:56:43 -0300

    Add lazy loading for images in the novel reader

--- Web.Portal/Others/Utilities.cs
+++ Web.Portal/Others/Utilities.cs
@@ -1,5 +1,8 @@
-﻿using System.Text;
+﻿using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
 using Microsoft.AspNetCore.WebUtilities;
 
 namespace WeebReader.Web.Portal.Others
@@ -11,5 +14,25 @@ namespace WeebReader.Web.Portal.Others
         public static string DecodeFromBase64(this string? source) => source == null ? string.Empty : Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(source));
 
         public static string RemoveHtmlTags(this string? source) => source == null ? string.Empty : Regex.Replace(source, "<.*?>", string.Empty);
+
+        public static string AddImgLazyLoading(this string? source, string placeholderLocation)
+        {
+            if (source == null)
+                return string.Empty;
+            
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(source);
+            var imageNodes = htmlDocument.DocumentNode.SelectNodes("//img").ToArray();
+
+            Parallel.ForEach(imageNodes, node =>
+            {
+                var src = node.GetAttributeValue("src", string.Empty);
+
+                node.SetAttributeValue("data-src", src);
+                node.SetAttributeValue("src", placeholderLocation);
+            });
+
+            return htmlDocument.DocumentNode.OuterHtml;
+        }
     }
 }
