b711a86ff4bf66550267551c3b5869dfa9b70afb
Micael Valentim <lord.valentim@gmail.com>
2020-08-01 20:21:30 -0300

    Consolidation of NSFW Warning

---
+++ Web.Portal/Static/libs/weebreader.1.0.0/main/nsfw-warning.js
@@ -0,0 +1,8 @@
+﻿$(() => {
+    $("#titles,#titles-mobile").addClass("active");
+
+    $("#nsfw-confirm").on("click", () => {
+        setCookie("seek_nsfw_content", "true", 0);
+        location.reload();
+    });
+});
