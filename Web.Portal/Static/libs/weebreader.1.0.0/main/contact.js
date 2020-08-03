f34fcbd4ed1f1239c1a04bf0a4c3a17d10f60137
Micael Valentim <lord.valentim@gmail.com>
2020-08-03 18:45:40 -0300

    Consolidation of Contact page

---
+++ Web.Portal/Static/libs/weebreader.1.0.0/main/contact.js
@@ -0,0 +1,57 @@
+﻿$(() =>{
+    let messageContainer = $(".form .message");
+    let form = $(".form");
+
+    $("#contact,#contact-mobile").addClass("active");
+
+    form.on("keypress", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            return false;
+        }
+    });
+
+    form.on("submit", () => tinyMCE.triggerSave()).form({
+        on: "blur",
+        fields: {
+            nickname: {
+                identifier: "nickname",
+                rules: [
+                    {
+                        type: "empty",
+                        prompt: nicknameRequired
+                    }
+                ]
+            },
+            email: {
+                identifier: "email",
+                rules: [
+                    {
+                        type   : "empty",
+                        prompt : emailRequired
+                    },
+                    {
+                        type: "email",
+                        prompt: validEmailRequired
+                    }
+                ]
+            },
+            message: {
+                identifier: "message",
+                rules: [
+                    {
+                        type: "empty",
+                        prompt: messageRequired
+                    }
+                ]
+            }
+        }
+    }).api({
+        url: requestRoute,
+        serializeForm: true,
+        method: "POST",
+        onSuccess: () => window.location.reload(),
+        onFailure: response => Array.isArray(response.messages) ? addError(messageContainer, response) : addError(messageContainer, buildFakeResponse([requestFailed])),
+        onError: () => addError(messageContainer, buildFakeResponse([requestFailed]))
+    });
+});
